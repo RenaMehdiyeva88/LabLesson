ec1ab0c5ae28931be932cdd9d25942cec5c762b7
Emil <emmehdiyev@yahoo.com>
2025-11-16 19:14:39 +0400

    hello

---
+++ LabLesson/LabLesson/Program.cs
@@ -0,0 +1,87 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Enter fuel consumption per 1 km: ");
+        //double literPerKm = double.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter total distance(km): ");
+        //double distanceKm = double.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter speed(km/h): ");
+        //double speedKmH = double.Parse(Console.ReadLine());
+        //double totalLiters = literPerKm * distanceKm;
+        //double timeHours = distanceKm / speedKmH;
+        //Console.WriteLine("Fuel needed: " + totalLiters + " liters");
+        //Console.WriteLine("Travel time: " + timeHours + " hours");
+
+        //Console.WriteLine("Enter how many minutes it takes to edit one photo: ");
+        //double minutesPerPhoto = double.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter the total number of photos: ");
+        //int photoCount = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter the photograher's hourly rate: ");
+        //double hourlyRate = double.Parse(Console.ReadLine());
+        //double totalMinutes = minutesPerPhoto * photoCount;
+        //double totalHours = totalMinutes / 60;
+        //double totalCost = totalHours * hourlyRate;
+        //Console.WriteLine("Total editing time(minutes): " + totalMinutes);
+        //Console.WriteLine("Total editing time(hours): " + totalHours);
+        //Console.WriteLine("Total cost: " + totalCost);
+
+        //Console.WriteLine("Enter the total amount of coffee in grams: ");
+        //double totalGrams = double.Parse(Console.ReadLine());
+        //double cups = totalGrams / 10;
+        //double income = cups * 3;
+        //Console.WriteLine("Maximum numbers of cups: " +(int)cups);
+        //Console.WriteLine("Total income if all cups are sold: " + income +" manats");
+
+        //Console.WriteLine("Enter total revenue: ");
+        //double totalRevenue = double.Parse(Console.ReadLine());
+        //double pricePerBar = 3.0;
+        //double barsSold = totalRevenue / pricePerBar;
+        //double gramsPerBar = 150.0;
+        //double totalIngredientsNeeded = barsSold * gramsPerBar;
+        //Console.WriteLine("Number of chocolate bars sold: " + barsSold);
+        //Console.WriteLine("Total ingredients needed: " + (int)totalIngredientsNeeded + " grams");
+
+        //Console.WriteLine("Enter the number of rows in the cinema hall: ");
+        //int numberOfRows = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter the number of seats per now: ");
+        //int seatsPerNow = int.Parse(Console.ReadLine());
+        //int totalSeats = numberOfRows * seatsPerNow;
+        //int ticketPrice = 5;
+        //int totalRevenue = totalSeats * ticketPrice;
+        //Console.WriteLine("\nCinema hall information: ");
+        //Console.WriteLine("Number of rows: " + numberOfRows);
+        //Console.WriteLine("Seats per row: " + seatsPerNow);
+        //Console.WriteLine("Total seats in the hall: " + totalSeats);
+        //Console.WriteLine("Total revenue if all seats are sold: " + totalRevenue + " units");
+
+        //Console.WriteLine("Enter the original price: ");
+        //double originalPrice = double.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter the discounted price: ");
+        //double discountedPrice = double.Parse(Console.ReadLine());
+        //double discountAmount = originalPrice - discountedPrice;
+        //double discountedPercentage = (discountAmount / originalPrice) * 100;
+        //Console.WriteLine("Discount percentage: " + discountedPercentage.ToString("F2") + "%");
+
+        //Console.WriteLine("Enter last year profit: ");
+        //double lastYearProfit = double.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter this year profit: ");
+        //double thisYearProfit = double.Parse(Console.ReadLine());
+        //double growthPercentange = (thisYearProfit - lastYearProfit) / (lastYearProfit) * 100;
+        //double nextYearProfit = thisYearProfit * (1 + growthPercentange / 100);
+        //Console.WriteLine("Growth percentage: " + growthPercentange.ToString("F2") + "%");
+        //Console.WriteLine("Next year's forecast profit: " + nextYearProfit.ToString("F2"));
+
+        //Console.WriteLine("Enter monthly salary: ");
+        //double salary = double.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter monthly expenses: ");
+        //double expenses = double.Parse(Console.ReadLine());
+        //double monthlySavings = salary - expenses;
+        //double savingsPercentage = (monthlySavings / salary) * 100;
+        //double yearlySavings = monthlySavings * 12;
+        //Console.WriteLine("Monthly savings: " + monthlySavings.ToString("F2") + "%");
+        //Console.WriteLine("Savings percentages: " + savingsPercentage.ToString("F2") + "%");
+        //Console.WriteLine("Yearly savings: " + yearlySavings.ToString("F2") + "%");
+
+    }
+}
