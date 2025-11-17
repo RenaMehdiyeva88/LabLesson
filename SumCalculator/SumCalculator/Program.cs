f719d54ed56e6d89e3a7956617a01317036f2b11
Emil <emmehdiyev@yahoo.com>
2025-11-17 09:57:57 +0400

    completed

---
+++ SumCalculator/SumCalculator/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Start program!");
+Console.WriteLine("Enter the first input: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second input: ");
+int b = int.Parse(Console.ReadLine());
+int c = a + b;
+Console.WriteLine("Your result is : " + c);
